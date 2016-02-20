76feb019ee503bfa0e6030a4ebc31ed46564c91d
Hector Huertas <hectorhuertas@gmail.com>
2016-02-20 11:02:36 -0700

    Initial commit

---
+++ test/controllers/page_controller_test.exs
@@ -0,0 +1,8 @@
+defmodule RockPaperScissors.PageControllerTest do
+  use RockPaperScissors.ConnCase
+
+  test "GET /", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+  end
+end
